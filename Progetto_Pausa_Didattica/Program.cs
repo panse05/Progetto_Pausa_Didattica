1e52f77c4884d9d6ce65b1a371e23e62fabb76f9
daniel panseri <panseridaniel@gmail.com>
2023-01-09 09:15:17 +0100

    Aggiungere i file di progetto.

---
+++ Progetto_Pausa_Didattica/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Progetto_Pausa_Didattica
+{
+    internal class Program
+    {
+        
+
+        // Codice dell'intervento (3 caratteri maiuscoli e 3 cifre)
+        public string Codice { get; set; }
+
+        // Data di acquisizione del prodotto
+        public DateTime DataAcquisto { get; set; }
+
+        // Data di consegna
+        public DateTime DataConsegna { get; set; }
+
+        // Descrizione del difetto
+        public string Difetto { get; set; }
+
+        // Descrizione dell'intervento effettuato
+        public string DescrizioneIntervento { get; set; }
+
+        // Prezzo orario
+        public decimal PrezzoOrario { get; set; }
+
+        // Ore di lavoro necessarie alla riparazione
+        public int OreLavoro { get; set; }
+
+        // Metodo per il calcolo del prezzo dell'intervento
+        public decimal CalcolaPrezzo()
+        {
+            return PrezzoOrario * OreLavoro;
+        }
+
+        // Metodo per l'incremento del costo orario in percentuale
+        public void IncrementaPrezzoOrario(decimal percentuale)
+        {
+            PrezzoOrario += PrezzoOrario * percentuale / 100;
+        }
+
+        // Metodo per il confronto tra due interventi
+        public int Confronta(Program altroIntervento)
+        {
+            return CalcolaPrezzo().CompareTo(altroIntervento.CalcolaPrezzo());
+        }
+
+    }
+}
+ 
---
+++ Progetto_Pausa_Didattica/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Le informazioni generali relative a un assembly sono controllate dal seguente 
+// set di attributi. Modificare i valori di questi attributi per modificare le informazioni
+// associate a un assembly.
+[assembly: AssemblyTitle("Progetto_Pausa_Didattica")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Progetto_Pausa_Didattica")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Se si imposta ComVisible su false, i tipi in questo assembly non saranno visibili
+// ai componenti COM. Se è necessario accedere a un tipo in questo assembly da
+// COM, impostare su true l'attributo ComVisible per tale tipo.
+[assembly: ComVisible(false)]
+
+// Se il progetto viene esposto a COM, il GUID seguente verrà utilizzato come ID della libreria dei tipi
+[assembly: Guid("9963b124-dc6d-4de2-af04-322b2256b1ee")]
+
+// Le informazioni sulla versione di un assembly sono costituite dai seguenti quattro valori:
+//
+//      Versione principale
+//      Versione secondaria
+//      Numero di build
+//      Revisione
+//
+// È possibile specificare tutti i valori oppure impostare valori predefiniti per i numeri relativi alla revisione e alla build
+// usando l'asterisco '*' come illustrato di seguito:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
